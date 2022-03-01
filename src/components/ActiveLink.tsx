782f6366460a46490d1542ea00c048ae1176adfc
Bruno Dias <bruno.fera09@gmail.com>
2022-03-01 18:24:59 -0300

    new...

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -1,14 +1,30 @@
 import Link, { LinkProps } from "next/link";
 import { cloneElement, ReactElement } from "react";
+import { useRouter } from "next/router";
 
 interface ActiveLinkProps extends LinkProps {
   children: ReactElement;
+  shouldMatchExactHref?: boolean;
 }
 // receber apenas um componente -> usar react.element
 // receber um componente que pode conter outro dentro, ou um texto ou numero -> usar react.node
 
-export function ActiveLink({ children, ...rest }: ActiveLinkProps) {
-  const isActive = true;
+export function ActiveLink({
+  children,
+  shouldMatchExactHref = false,
+  ...rest
+}: ActiveLinkProps) {
+  let isActive = false;
+  const { asPath } = useRouter();
+  if (shouldMatchExactHref && (asPath === rest.href || rest.as)) {
+    isActive = true;
+  }
+  if (
+    !shouldMatchExactHref && (asPath.startsWith(String(rest.href))) ||
+    asPath.startsWith(String(rest.as))
+  ) {
+    isActive = true;
+  }
   return (
     <Link {...rest}>
       {cloneElement(children, {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
 import { Flex, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
 
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = (value) => {
+    console.log(value);
+  };
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -12,13 +23,29 @@ export default function SignIn() {
         p="8"
         borderRadius="8"
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" label="email" type="email" />
-          <Input name="password" label="password" type="password" />
+          <Input
+            name="email"
+            label="E-mail"
+            type="email"
+            {...register("email")}
+          />
+          <Input
+            name="password"
+            label="Senha"
+            type="password"
+            {...register("password")}
+          />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          isLoading={formState.isSubmitted}
+        >
           Entrar
         </Button>
       </Flex>
